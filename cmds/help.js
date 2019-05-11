b4d4bc8812a6a7c9df02dba28f52a178997ebc70
tailsjs <49571184+tailsjs@users.noreply.github.com>
2019-05-11 22:19:55 +0300

    Добавил версию бота

--- cmds/help.js
+++ cmds/help.js
@@ -3,6 +3,7 @@ module.exports = {
   func: async(msg, { botN, cmds }) => {
     let result = [
       `Команды бота ${botN}.`,
+      `Версия бота: v` + `${cmds.length}`.split('').join('.'),
       ``,
       `&#128313; Команды для Пользователей &#128313;`,
       cmds
