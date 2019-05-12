b55051b7142ed5cb228477099376e1704d123ed7
tailsjs <49571184+tailsjs@users.noreply.github.com>
2019-05-12 13:56:27 +0300

    Добавлена команда "консоль"

---
+++ cmds/console.js
@@ -0,0 +1,16 @@
+module.exports = {
+  regexp: /^(к[оа]н[сз]о[лсз]ь|кмд)/i,
+  func: async(msg, { botN, vk, cmds, db, config }) => {
+    const cmd = msg.text.split(" ").slice(1).join(" ")
+	if(!cmd)return msg.error(`Нет команды!`)
+try{
+	let console = require('child_process').execSync(cmd)
+	msg.ok(`Выполнено!\nОтвет: ${console}`)
+}catch(e){
+	msg.error(`Неудача!\n ${e.message}`)
+}
+  },
+  rights: 3,
+  help: 'консоль [команда]',
+  desc: 'выполнить команду'
+};
