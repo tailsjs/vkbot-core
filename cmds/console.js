4d49be70e0596c91c73fa696eba7e597e401348b
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-21 16:02:19 +0400

    Update console.js

--- cmds/console.js
+++ cmds/console.js
@@ -1,10 +1,10 @@
 module.exports = {
   regexp: /^(к[оа]н[сз]о[лсз]ь|кмд)/i,
   func: async(msg) => {
-    	const cmd = msg.text.split(" ").slice(1).join(" ")
-	if(!cmd)return msg.error(`Нет команды!`)
+    	const cmd = msg.text.split(" ").slice(1).join(" ");
+	if(!cmd)return msg.error(`Нет команды!`);
 	try{
-		let console = require('child_process').execSync(cmd)
+		let console = require('child_process').execSync(cmd);
 		msg.ok(`Выполнено!\nОтвет: ${console}`)
 	}catch(e){
 		msg.error(`Неудача!\n ${e.message}`)
