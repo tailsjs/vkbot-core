b19c27c69b0e70b429f9e6c8dc853c713a29a1c5
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-21 16:05:54 +0400

    Update unWarn.js

--- cmds/unWarn.js
+++ cmds/unWarn.js
@@ -3,8 +3,8 @@ module.exports = {
     func: async(msg, { db }) => {
         let ID = msg.fwds[0] ? msg.fwds[0].senderId : msg.text.split(' ')[1];
         if (!ID || ID < 0 || isNaN(ID)) return msg.error('Укажите правильный айди'); // Если айди не указан или айди меньше нуля (группа) или айди не равен числу, то пишем, что нужно указать айди.
-	if(ID === msg.senderId)return msg.error('Вы не можете убрать у себя предупреждение!')
-        let user = await db.getUser(ID);
+	if(ID === msg.senderId)return msg.error('Вы не можете убрать у себя предупреждение!');
+        let user = await db.getUser(Number(ID));
         if(user.warns === 0)return msg.error('У пользователя нет предупреждений!')
 
         user.warns -= 1
