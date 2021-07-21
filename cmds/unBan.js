7209ef44fb3c64ac2517be23081130c12ef927c5
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-21 16:04:20 +0400

    Update unBan.js

--- cmds/unBan.js
+++ cmds/unBan.js
@@ -3,18 +3,18 @@ module.exports = {
     func: async(msg, { db }) => {
         let ID = msg.fwds[0] ? msg.fwds[0].senderId : msg.text.split(' ')[1];
         if (!ID || ID < 0 || isNaN(ID)) return msg.error('Укажите правильный айди'); // Если айди не указан или айди меньше нуля (группа) или айди не равен числу, то пишем, что нужно указать айди.
-        let user = await db.getUser(ID);
+        let user = await db.getUser(Number(ID));
         if (!user.ban.isBanned) return msg.error('Этот пользователь уже разбанен');
 
         user.ban.isBanned = false;
         user.ban.reason = "";
-        user.warns = 0
+        user.warns = 0;
         db.write();
 
         msg.ok(`Пользователь [id${user.id}|${user.nick}] успешно разбанен`);
         msg.warn(`Вас разблокировал ${msg.rights[msg.user.rights]} ${msg.user.nick}`, {
-			user_id: ID
-		})
+		user_id: ID
+	})
     },
     rights: 2, // Команда для Админов и выше
     help: 'разбан [айди]',
