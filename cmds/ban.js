88bb1c9ff3d46fb8903956d87a4927e1da4dae8e
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-07-21 16:03:03 +0400

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -2,13 +2,13 @@ module.exports = {
     tag: ['ban', 'бан', 'забанить', 'забань'],
     func: async(msg, { db }) => {
         let ID = msg.fwds[0] ? msg.fwds[0].senderId : msg.text.split(' ')[1];
-		if(ID === msg.senderId)return msg.error(`Вы не можете забанить себя!`)
+	if(ID === msg.senderId)return msg.error(`Вы не можете забанить себя!`)
         let reason = (msg.fwds[0] ? msg.text.split(' ').slice(1).join(' ') : msg.text.split(' ').slice(2).join(' ')).replace(/\n/g, " ");
         if (!ID || ID < 0 || isNaN(ID)) return msg.error('Укажите правильный айди'); // Если айди не указан или айди меньше нуля (группа) или айди не равен числу, то пишем, что нужно указать айди.
         if (reason.length > 20) return msg.error('Укажите причину бана длиной не больше 20 символов');
         if(!reason){ reason = "Нарушение правил." }
 
-        let user = await db.getUser(ID);
+        let user = await db.getUser(Number(ID));
         if (user.ban.isBanned) return msg.error('Этот пользователь уже забанен');
 
         user.ban.isBanned = true;
@@ -18,7 +18,7 @@ module.exports = {
         msg.ok(`Пользователь [id${user.id}|${user.nick}] успешно забанен по причине: "${reason}"`);
         msg.warn(`Вас заблокировал ${msg.rights[msg.user.rights]} ${msg.user.nick} по причине: "${reason}"`, {
 			user_id: ID
-		})
+	})
     },
     rights: 2, // Команда для Админов и выше
     help: 'бан [айди]',
